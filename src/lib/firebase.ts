d06cbcab72b35cc83c0cfb0cb45c91d134bb4833
estela diaz <esteladiaz9010@gmail.com>
2025-08-11 18:12:16 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
@@ -5,14 +6,13 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
+// This configuration is simplified to the essentials for Auth, Firestore, and Storage.
 const firebaseConfig = {
   apiKey: "AIzaSyAGbLMGcxSRumk--pywW6PvytcTwRn4j1E",
   authDomain: "teereserve-golf.firebaseapp.com",
-  projectId: "teereserve-golf",
   storageBucket: "teereserve-golf.appspot.com",
   messagingSenderId: "502212139547",
-  appId: "1:502212139547:web:37ebd5c12071689b20b6be",
-  measurementId: "G-HYV3VCD0WW"
+  appId: "1:502212139547:web:37ebd5c12071689b20b6be"
 };
 
 
