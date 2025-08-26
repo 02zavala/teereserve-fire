501dc71423a8dc734838f2d234b5a3ce90d4ecfb
estela diaz <esteladiaz9010@gmail.com>
2025-08-26 04:42:05 +0000

    sigue starting

--- src/middleware.ts
+++ src/middleware.ts
@@ -36,6 +36,8 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  // Matcher ignoring `/_next/` and `/api/`
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|logo.svg|.*\\.jpg$).*)'],
-};
+  // Matcher ignoring `/_next/`, `/api/` and static files
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico|logo.svg|.*\\.jpg$).*)',
+  ],
+}
